dff53ed478ab8bc6f160121c14e69cd13ea73fe1
jotha04 <jonatan-041204@hotmail.es>
2024-11-10 16:03:04 -0500

    Se actualizo la encuesta en su totalidad y se le coloco un boton de devolver

--- encuesta.php
+++ encuesta.php
@@ -10,14 +10,41 @@
     <div class="container">
         <h1>Formulario de Encuesta</h1>
         <form action="procesar_encuesta.php" method="post">
-            <label for="localidad">Localidad o Municipio:</label>
-            <input type="text" id="localidad" name="localidad" required>
-
-            <label for="infraestructura">Infraestructura Existente:</label>
-            <input type="text" id="infraestructura" name="infraestructura" required>
-
-            <label for="poblacion">Población:</label>
-            <input type="number" id="poblacion" name="poblacion" required>
+           <label for="zona">Localidad o municipio:</label>
+            <select name="zona" id="zona" required>
+                <option value="">Selecciona una zona...</option>
+                <option value="algarrobo">Algarrobo</option>
+                <option value="aracataca">Aracataca</option>
+                <option value="el dificil">El Difícil (Ariguaní)</option>
+                <option value="cerro de san antonio">Cerro de San Antonio</option>
+                <option value="chibolo">Chibolo</option>
+                <option value="cienaga">Ciénaga</option>
+                <option value="concordia">Concordia</option>
+                <option value="el banco">El Banco</option>
+                <option value="el pinon">El Piñón</option>
+                <option value="el reten">El Retén</option>
+                <option value="fundacion">Fundación</option>
+                <option value="guamal">Guamal</option>
+                <option value="nueva granada">Nueva Granada</option>
+                <option value="pedraza">Pedraza</option>
+                <option value="pijino del carmen">Pijiño del Carmen</option>
+                <option value="pivijay">Pivijay</option>
+                <option value="plato">Plato</option>
+                <option value="pueblo viejo">Pueblo Viejo</option>
+                <option value="remolino">Remolino</option>
+                <option value="san angel">San Ángel</option>
+                <option value="salamina">Salamina</option>
+                <option value="san sebastian de buenavista">San Sebastián de Buenavista</option>
+                <option value="santa ana">Santa Ana</option>
+                <option value="santa barbara de pinto">Santa Bárbara de Pinto</option>
+                <option value="santa marta">Santa Marta (Distrito)</option>
+                <option value="san zenon">San Zenón</option>
+                <option value="sitionuevo">Sitionuevo</option>
+                <option value="tenerife">Tenerife</option>
+                <option value="punta de piedra">Punta de Piedra (Zapayán)</option>
+                <option value="prado sevilla">Prado Sevilla</option>
+                <option value="Otro">Otro</option>
+            </select>
 
             <label for="proyecto">Proyecto en Curso:</label>
             <input type="text" id="proyecto" name="proyecto" required>
@@ -42,9 +69,13 @@
                 <option value="activo">Activo</option>
                 <option value="en proceso">En Proceso</option>
                 <option value="finalizado">Finalizado</option>
+                <option value="espera">A la espera</option>
             </select>
 
-            <button type="submit">Enviar Reporte</button>
+            <div class="button-group">
+                <button type="submit" name='enviar'>Enviar</button>
+                <button type="button" onclick="window.history.back();">Devolver</button>
+            </div>
         </form>
     </div>
 </body>
